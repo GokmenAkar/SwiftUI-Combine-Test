6a2ba09ddbe9b21c562c9dca64379ec4f916cef9
GokmenAkar <gokmen.akar92@gmail.com>
2020-03-01 11:28:57 +0300

    - modular architecture

---
+++ Features/Articles/Article/Article/Article.h
@@ -0,0 +1,19 @@
+//
+//  Article.h
+//  Article
+//
+//  Created by GÖKMEN AKAR on 27.02.2020.
+//  Copyright © 2020 GÖKMEN AKAR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Article.
+FOUNDATION_EXPORT double ArticleVersionNumber;
+
+//! Project version string for Article.
+FOUNDATION_EXPORT const unsigned char ArticleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Article/PublicHeader.h>
+
+
